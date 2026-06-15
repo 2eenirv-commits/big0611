bfbd37144b1be02887a5c89deb2992f10ae4c6fe
2ee <2eenirv@gmail.com>
2026-06-15 17:49:51 +0900

    리스트

---
+++ py/p01/basic/ch02/ex02_리스트.py
@@ -0,0 +1,23 @@
+# 비효율적인 방법
+#ctrl+d: 같은 텍스트를 선택
+fruit1 = '사과'
+fruit2 = '바나나'
+fruit3 = '오렌지'
+
+
+# 컨테이너 자료형
+# : 리스트, 튜플, 세트, 딕셔너리
+# 효율적인 방법
+fruits = ['사과', '바나나', '오렌지']
+print(fruits)
+
+fruits2 = ['사과','바나나','오렌지','사과','바나나']
+print(fruits2)
+
+# 아이템 선택
+# 인덱싱
+print(fruits[0])
+print(fruits[-1])
+# 슬라이싱
+print(fruits[1:3])
+
